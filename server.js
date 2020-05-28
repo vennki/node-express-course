c583d49ce1702972ec33c05e11fb966759ef39e2
Venkat <vennki9@gmail.com>
2020-05-28 19:53:07 +0530

    add first GET route

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const app = express();
 app.listen(8000, function(){
     console.log('server is running...');
 })
+
+
+const mockUserData = [
+    {name: 'Venkat'},
+    {name: 'Trishika'}
+];
+
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    });
+});
+
