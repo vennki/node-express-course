7403844c87a6311696981f1bf7fded2966b5ab13
Venkat <vennki9@gmail.com>
2020-05-28 20:08:07 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.listen(8000, function(){
     console.log('server is running...');
-})
-
+});
 
 const mockUserData = [
     {name: 'Venkat'},
     {name: 'Trishika'}
 ];
+
+app.use(bodyParser.json());
+
 
 app.get('/users', (req, res) => {
     res.json({
@@ -25,5 +28,26 @@ app.get('/users/:id', (req, res) => {
         success: true,
         message: 'got one user',
         user: req.params.id
-    })
+    });
+});
+
+app.post('/login', (req, res) => {
+    const userName = req.body.userName;
+    const password = req.body.password;
+
+    const mockUserName = 'Venkat';
+    const mockPassword = 'Password';
+
+    if(userName === mockUserName && mockPassword === mockPassword){
+        res.json({
+            success: true,
+            message: 'UserName and Password match!',
+            token: 'encrypted token goes here'
+        });
+    } else{
+        res.json({
+            success: false,
+            message: 'UserName and Password do not match'
+        })
+    }
 });
